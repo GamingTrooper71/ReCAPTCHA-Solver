f46d37e3d1fa4b5c3ba4d9711d704b0ab1323454
verdict <65512475+notverdict@users.noreply.github.com>
2020-08-06 02:04:54 -0500

    ReCAPTCHA-Solver

--- main.py
+++ main.py
@@ -71,6 +71,8 @@ class ReCaptcha:
         WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it(
             (By.CSS_SELECTOR, "iframe[title='recaptcha challenge']")))
 
+        time.sleep(3)
+        
         driver.find_element_by_css_selector("button[class='rc-button goog-inline-block rc-button-audio']").click()
 
         time.sleep(3)
